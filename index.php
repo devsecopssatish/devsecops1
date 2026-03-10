e3d380e103841b79b7cab4eb92f22365081cec7d
devsecopssatish <devsecops.satish@gmail.com>
2026-03-10 07:32:15 +0530

    Remove dummy AWS credentials from index.php
    Removed dummy secrets from index.php

--- index.php
+++ index.php
@@ -1,3 +1,6 @@
 <?php
 echo $_GET['test'];
+//dummy secrets 
+//  private val aws_access_key_id = "AKIAX56QKKOLPQ7G7ABC"
+    // private val aws_secret_access_key = "OviCwsFNWeoCSDKl3ZoD8j4BPnc1kCsfV+lOABCw"
 ?>
